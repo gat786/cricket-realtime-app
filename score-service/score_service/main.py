8a54136877f0dac0ade28438c2bab8904dea4be8
Ganesh Tiwari <ganesht049@gmail.com>
2023-11-25 17:49:44 +0530

    feat: initial app created

--- score-service/score_service/main.py
+++ score-service/score_service/main.py
@@ -1,4 +1,21 @@
 from .utils import setup
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+  return {
+    "message": "Hello World"
+  }
+
 
 def main():
-  print("Hello World!")
+  uvicorn.run(
+    "score_service.`main:app",
+    host="0.0.0.0",
+    port=8000,
+    reload=True,
+  )
