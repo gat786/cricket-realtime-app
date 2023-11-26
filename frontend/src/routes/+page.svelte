ba5f2c4b6e10f4e72a1b69768933a28bd438f87d
Ganesh Tiwari <ganesht049@gmail.com>
2023-11-26 14:23:21 +0530

    feat: added frontend sveltekit bootstrap component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script>
+
+	import { PUBLIC_SCORE_SERVICE_ENDPOINT } from "$env/static/public";
+
+	const get_matches = () => {
+
+		fetch('/api/matches')
+			.then(res => res.json())
+			.then(matches => {
+				console.log(matches);
+			});
+	}
+
 </script>
 
 <svelte:head>
@@ -7,6 +19,7 @@
 </svelte:head>
 
 <section>
+	This is my home section
 </section>
 
 <style>
