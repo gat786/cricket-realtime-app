48efeb96840ed75b9d061022acaa7776e8fe4b01
Ganesh Tiwari <ganesht049@gmail.com>
2023-11-28 10:07:33 +0530

    feat: added changes to player service to allow origins

--- player-service/player_service/main.py
+++ player-service/player_service/main.py
@@ -3,11 +3,16 @@ from player_service.utils import exports
 
 import uvicorn
 import logging
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from player_service.utils import search
 
+origins = [
+  "http://localhost",
+  "http://localhost:5173"
+]
 app     = FastAPI()
 port    = 8000
 logger  = logging.getLogger(__name__)
@@ -39,6 +44,14 @@ def get_player_from_name(player_search_query: PlayerSearchQuery):
   return {
     "players": closest_matches.to_dict(orient="records")
   }
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 def run_app():
   uvicorn.run(
