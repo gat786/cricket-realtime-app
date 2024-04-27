02663c961888fade5e4f971d06c64d0003ae2380
Ganesh Tiwari <ganesht049@gmail.com>
2024-04-27 23:41:57 +0530

    feat: dropped svelte frontend app

--- apps/frontend/src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
-
-import { PUBLIC_PLAYER_SERVICE_ENDPOINT } from '$env/static/public';
-
-const playerServiceEndpoint = PUBLIC_PLAYER_SERVICE_ENDPOINT;
