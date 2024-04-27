cf42b71cbebcf8f71de84ddb10766d6292c6bbd0
Ganesh Tiwari <ganesht049@gmail.com>
2024-04-27 23:41:04 +0530

    feat: switched directories and added a dockerfile to run it all together

--- frontend-blazor/Models/CountriesNames.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace frontend_blazor.Models {
-    public record CountriesNames(
-
-        [property: JsonPropertyName("countries")] IReadOnlyList<string> Countries
-    );
-}
--- frontend-blazor/Models/Message.cs
+++
@@ -1,7 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace frontend_blazor.Models {
-  public record Message (
-    [property: JsonPropertyName("message")] string MessageText
-  );
-}
