c59408cf8818717921baf826ea00baffe92d52ec
Ganesh Tiwari <ganesht049@gmail.com>
2024-08-17 16:35:23 +0530

    feat: improve player search and logging

--- apps/player-service/player_service/utils/search.py
+++ apps/player-service/player_service/utils/search.py
@@ -1,10 +1,19 @@
 from player_service.utils import exports
 import pandas as pd
 
+import logging
+
+logger = logging.getLogger()
 
 def search_from_file(player_name: str, country_name: str):
-  first_name, last_name = player_name.split(" ")
-  print(f"Searching for {first_name} {last_name}")
+  logger.debug(f"Player name supplied for searching: {[player_name]}")
+  names_split           = player_name.split(" ")
+  first_name, last_name = "", ""
+  if len(names_split) > 1:
+    first_name, last_name = names_split[0], names_split[-1]
+  else: 
+    first_name, last_name = names_split[0], ""
+  logger.debug(f"FirstName: {first_name}, Second Name: {last_name}")
   with open(f"{exports.data_root}/player/players_data.csv") as player_csv_file:
     df: pd.DataFrame = pd.read_csv(player_csv_file)
     df["country_name_search"] = df["country_name"].str.lower()
@@ -16,7 +25,7 @@ def search_from_file(player_name: str, country_name: str):
     
     first_name = [x for x in first_name]
     first_letter = first_name[0]
-    print(f"Searching for first letter - {first_letter}")
+    logger.debug(f"Searching for first letter - {first_letter}")
     closest_match = same_lastname[same_lastname["firstname_search"].str.startswith(first_letter)]
     
     closest_match.drop(columns=["country_name_search", "firstname_search", "lastname_search"], inplace=True)
--- apps/player-service/player_service/utils/setup.py
+++ apps/player-service/player_service/utils/setup.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
+import os
 import logging
 
 load_dotenv()
 
+logLevel = os.getenv("LOGLEVEL", 10)
+
 logging.basicConfig(
-  level=logging.INFO,
+  level=logLevel,
   format='%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s',
   datefmt='%Y-%m-%d %H:%M:%S'
 )
